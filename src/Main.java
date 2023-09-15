af03dcc42b43c48676f73dfab59810c40a40d5c3
CSC1060-Marsh <143460025+CSC1060-Marsh@users.noreply.github.com>
2023-09-15 17:26:48 -0600

    Added Descriptions

--- src/Main.java
+++ src/Main.java
@@ -24,20 +24,35 @@ public class Main {
             System.out.println("Your letter grade is a 'B+'.");
             //This defines the grade "B" as above or equal to 82.00.
         } else if (grade >= 82.00) {
+            //This outputs the letter grade "B" if the above statement is true.
             System.out.println("Your letter grade is a 'B'.");
+            //This defines the grade "B-" as above or equal to 79.00.
         } else if (grade >= 79.00) {
+            //This outputs the letter grade "B-" if the above statement is true.
             System.out.println("Your letter grade is a 'B-'.");
+            //This defines the grade "C+" as above or equal to 77.00.
         } else if (grade >= 77.00) {
+            //This outputs the letter grade "C+" if the above statement is true.
             System.out.println("Your letter grade is a 'C+'.");
+            //This defines the grade "C" as above or equal to 72.00.
         } else if (grade >= 72.00) {
+            //This outputs the letter grade "C" if the above statement is true.
             System.out.println("Your letter grade is a 'C'.");
+            //This defines the grade "C-" as above or equal to 69.00.
         } else if (grade >= 69.00) {
+            //This outputs the letter grade "C-" if the above statement is true.
             System.out.println("Your letter grade is a 'C-'.");
+            //This defines the grade "D+" as above or equal to 67.00.
         } else if (grade >= 67.00) {
+            //This outputs the letter grade "D+" if the above statement is true.
             System.out.println("Your letter grade is a 'D+'.");
+            //This defines the grade "D" as above or equal to 60.00.
         } else if (grade >= 60.00) {
+            //This outputs the letter grade "D" if the above statement is true.
             System.out.println("Your letter grade is a 'D'.");
+            //This sets the automatic response if all other "if" tests fail.
         } else {
+            //This outputs the letter grade "F" if all other "if" tests fail.
             System.out.println("Your letter grade is an 'F'.");
         }
     }
